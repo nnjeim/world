13b986ad6cbebf299084022e495357f7587f8863
geoffrey <geoffrey@renaissance.nl>
2022-03-26 13:23:40 +0100

    Add dutch localization
    Signed-off-by: geoffrey <geoffrey@renaissance.nl>

---
+++ resources/lang/nl/response.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+	'errors' => [
+		'server_error' => 'Internal serverfout',
+		'record_not_found' => 'Geen :attribute kon gevonden worden!',
+	],
+	'attributes' => [
+		'phone' => 'telefoon|telefoons',
+		'country' => 'land|landen',
+		'city' => 'plaats|plaatsen',
+		'state' => 'staat|staten',
+		'timezone' => 'tijdzone|tijdzones',
+		'currency' => 'munteenheid|munteenheden',
+		'language' => 'taal|talen',
+	],
+];
