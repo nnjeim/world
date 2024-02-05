0567fb6fd4f903e0e2d43de8e5a355081776577c
Gennaro Landolfi <gennarolandolfi@rinodrummer.dev>
2024-02-05 14:58:25 +0100

    Fixed replacement in response for Italian with slight form improvement

--- resources/lang/it/response.php
+++ resources/lang/it/response.php
@@ -3,7 +3,7 @@
 return [
 	'errors' => [
 		'server_error' => 'Errore interno del server',
-		'record_not_found' => 'No: attributo trovato!',
+		'record_not_found' => 'Nessun elemento di :attribute trovato!',
 	],
 	'attributes' => [
 		'phone' => 'telefono|telefoni',
