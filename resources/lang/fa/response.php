e3b5d643394b2557cc84605142040082a9cf6435
Ali <97373457+DevNull-IR@users.noreply.github.com>
2024-07-10 17:40:48 +0330

    Create response.php
    Add Persian language

---
+++ resources/lang/fa/response.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'exceptions' => [
+        'server_error' => 'خطا در سرور',
+        'record_not_found' => 'رکورد :attribute یافت نشد!',
+    ],
+    'attributes' => [
+        'phone' => 'تلفن‌ها|تلفن',
+        'country' => 'کشورها|کشور',
+        'city' => 'شهرها|شهر',
+        'state' => 'استان‌ها|استان',
+        'timezone' => 'مناطق زمانی|منطقه زمانی',
+        'currency' => 'ارزها|ارز',
+        'language' => 'زبان‌ها|زبان',
+    ],
+];
